5eb68fdf0c76cbb5a853b1d3d36d1e03effc46b2
alexandrekcosta <xandykelvin@gmail.com>
2023-04-17 04:28:20 -0300

    A file has been edited so new content has been added

--- cities.py
+++ cities.py
@@ -1,40 +1,23 @@
-co = str(input("Enter city name(4 options:Mumbai,Belagua,Shrasvati,Kishoreganj): "))
+cname = str(input("Enter name of country: \n"))
+gdp = int(input("Enter GDP of country: \n"))
+city = str(input("Enter a city name: \n"))
+gdpPC = int("Enter GDP per capita of {}\n".format(city))
+population = int(input("Enter population of {}\n".format(city)))
+areakm2 = int(input("Enter area per square of {}\n:".format(city)))
 
-if co == "Mumbai":
-    print("COUNTRY:INDIA/IN\n"
-          "GDP:$3.737 trillion\n"
-          "CITY:MUMBAI\n"
-          "GDP per capita:$606.625 billion\n")
-
-elif co == "Belagua":
-    print("COUNTRY:BRAZIL/BR\n"
-          "GDP:$1.810 billion\n"
-          "CITY:BELAGUA\n"
-          "GDP per capita:R$6.920\n")
-
-elif co == "Shrasvati":
-    print("COUNTRY:INDIA/IN\n"
-          "GDP:$3.737 trillion\n"
-          "CITY:SHRASVATI\n"
-          "GDP per capita:R$ 2\n")
-
-elif co == "Kishoreganj":
-    print("COUNTRY:BANGLADESH/BD\n"
-          "GDP:$421 billion\n"
-          "CITY:KISHOREGANJ\n"
-          "GDP per capita:R$85\n")
-
+if gdpPC <=2000 and population <= 20000 and areakm2 <= 100000:
+    print("{} is a city which is eligible for Extra Money for Municipal Fund!\n"
+          "DATA OF CITY\n"
+          "GDP per capita:{}\n"
+          "Population:{}\n"
+          "Area per square:{}\n"
+          "Country which city is located:{}\n"
+          "GDP of {}\n ".format(city,gdpPC,population,areakm2,cname,gdp))
 else:
-    print("Invalid input")
-
-gdpPCCondition = float(input("Enter GDP per capita value (no float point): \n"))
-
-if gdpPCCondition >= 2000:
-    print("Value input is above 2 thousands,so there is bellow a list of cities which DON'T deserve Extra Municipal Auxiliary Fund:\n"
-          "MUMBAI\n"
-          "BELAGUA\n")
-
-elif gdpPCCondition <= 2000:
-    print("Value input is bellow 2 thousands,so there is bellow a list of cities which DESERVE Extra Municipal Auxiliary Fund:\n"
-          "SHRASVATI\n"
-          "KISHOREGANJ\n")
+    print("{} is a city which is NOT eligible for Extra Money for Municipal Fund!\n"
+          "DATA OF CITY\n"
+          "GDP per capita:{}\n"
+          "Population:{}\n"
+          "Area per square:{}\n"
+          "Country which city is located:{}\n"
+          "GDP of {}\n ".format(city, gdpPC, population, areakm2, cname, gdp))
